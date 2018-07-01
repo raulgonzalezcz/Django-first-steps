2bc196496d19cd945605343283cbecc2317b8e46
raulgonzalezcz <raul.gonzalezcz@udlap.mx>
2018-07-01 17:01:44 -0500

    Adding to admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -5,6 +5,34 @@ from django.contrib import admin
 
 # Register your models here.
 # Make the poll app modifiable in the admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	#Simple arranging information
+    #fields = ['pub_date', 'question_text']
+    #Adding title (None = No title)
+    """
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    """
+    # Adding Choice input
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    #Show more information
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    #Add filter panel
+    list_filter = ['pub_date']
+    #Add search panel
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
